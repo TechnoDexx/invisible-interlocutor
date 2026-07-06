1dac02da6280369bc1833be184b47f9e9526dbb1
Igor Golov <itshark@mail.ru>
2026-07-06 07:07:59 +0300

    Add classes

--- core/ai_client.py
+++ core/ai_client.py
@@ -1,10 +1,26 @@
 from dotenv import load_dotenv
 class AIClient():
-    def __init__(self,api_key, base_url, project, prompt_id, retries, delay,history):
-        self.api_key=api_key
-        self.base_url=base_url
-        self.project=project
-        self.prompt_id=prompt_id
-        self.retries=retries
-        self.delay=delay
-        self.history=history
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv('API_KEY')
+        self.base_url = os.getenv('BASE_URL')
+        self.project = os.getenv('PROJECT')
+        self.prompt_id = os.getenv('PROMPT_ID')
+        self.client = openai.OpenAI(self.api_key,
+                                    self.base_url,
+                                    self.project)
+
+    def ask(self, history, retries=3):
+        for attempt in range(retries):
+            try:
+                response = self.client.responses.create(
+                    prompt={"id": self.prompt_id},
+                    input=history
+                )
+                answer = response.output_text
+            except Exception as e:
+                if attempt == retries-1:
+                    raise
+                time.sleep(1)
+                continue
+        return answer
